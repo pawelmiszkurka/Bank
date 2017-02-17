d034efd9e91c4f149f265c7ec5ec5e87a28dd68c
pmiszkurka <pawemiszkurka@github.com>
2017-02-17 09:41:51 +0000

    missing files

---
+++ src/BankAcoount.java
@@ -0,0 +1,45 @@
+/**
+ * Created by pmiszkurka on 16/02/2017.
+ */
+public class BankAcoount {
+
+
+    /**
+     * A bank account has a balance that can be changed
+     */
+    public BankAcoount(){
+        //implementaion
+    }
+
+    /**
+     * initialises the new object of type BankAccount with initial balance
+     * @param initialBalance sets the inniital ammount
+     */
+    public BankAcoount(double initialBalance){
+        //implementation
+    }
+    /**
+     * Deposits ammount into bank account
+     * @param ammount the ammount to deposit
+     */
+    public void deposit(double ammount){
+        //implementation
+    }
+
+    /**
+     * Withdraws money from the bank account
+     * @param ammount the ammount to withdraw
+     */
+    public void withdraw(double ammount){
+
+    }
+    
+    /**
+     * Gets the current balance of the bank acocunt
+     * @return the current balance
+     */
+    public double getBalance(){
+        //implementation
+    }
+
+}
